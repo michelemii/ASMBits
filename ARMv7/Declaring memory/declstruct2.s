12123d6dc57cdac390340479267354ee57069b39
Michele Missiroli <163904638+michelemissiroli@users.noreply.github.com>
2026-09-09 02:07:06 +0200

    Create declstruct2.s

---
+++ ARMv7/Declaring memory/declstruct2.s
@@ -0,0 +1,27 @@
+// Insert declarations here
+	
+.data
+Root:
+    .word Node10
+    .word Node11
+    .word 0
+
+Node10:
+    .word 0
+    .word 0
+    .word 10
+
+Node11:
+    .word Node20
+    .word Node21
+    .word 11
+
+Node20:
+    .word 0
+    .word 0
+    .word 20
+
+Node21:
+    .word 0
+    .word 0
+    .word 21
